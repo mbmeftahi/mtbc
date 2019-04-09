07a7c143ef0c482465268a411cd373f51e190ed0
Mohammad B Meftahi <mmeftahi@hotmail.com>
2019-04-09 10:18:00 -0500

    php GET anbd if else

--- php/hello.php
+++ php/hello.php
@@ -6,6 +6,13 @@ $msg1 = 'Hello  {$name}';
 $msg2 = "Hello  {$name}";
 $msg3 = 'Hello '.$name;
 
+$name = 'Bob Smith';
+$greeting = "Hello {$name}";
+$count=5;
+$price=6;
+$total=($count*$price);
+//Note the escape character before the dollar sign
+$msg = "I see you have {$count} oranges, that will be \${$total}";
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +23,9 @@ $msg3 = 'Hello '.$name;
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
+    <h1><?php echo $greeting; ?></h1>
+       <p><?php echo $msg;?></p>
+
        <h1><?php echo $msg1; ?></h1>
        <h2><?php echo $msg2; ?></h2>
        <h1><?php echo $msg3; ?></h1>
